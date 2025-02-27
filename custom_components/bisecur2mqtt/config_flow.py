d6f50bf42266a6264fa13d3f02d9abf492a99a94
Andrii Bratanich <andrey.bratanich@gmail.com>
2025-02-27 10:14:38 +0200

    init

--- custom_components/bisecur2mqtt/config_flow.py
+++ custom_components/bisecur2mqtt/config_flow.py
@@ -3,10 +3,8 @@ from homeassistant import config_entries
 from .const import DOMAIN
 
 class BisecurConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Настройка Bisecur2MQTT в UI."""
 
     async def async_step_user(self, user_input=None):
-        """Первый шаг конфигурации."""
         if user_input is not None:
             return self.async_create_entry(title="Bisecur2MQTT", data=user_input)
 
