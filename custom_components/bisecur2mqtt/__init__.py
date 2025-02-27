7187baea9a84a362289c201b27204994172c21d0
Andrii Bratanich <andrey.bratanich@gmail.com>
2025-02-27 20:18:57 +0200

    init

--- custom_components/bisecur2mqtt/__init__.py
+++ custom_components/bisecur2mqtt/__init__.py
@@ -18,21 +18,21 @@ def start_bisecur_service(config):
         # Передаём настройки в bisecur2mqtt.py через аргументы командной строки
         args = [
             "python3", script_path,
-            "--bisecur_user", config.get("bisecur_user", ""),
-            "--bisecur_pw", config.get("bisecur_pw", ""),
-            "--bisecur_ip", config.get("bisecur_ip", ""),
-            "--bisecur_mac", config.get("bisecur_mac", "FF:FF:FF:FF:FF:FF"),
-            "--src_mac", config.get("src_mac", "FF:FF:FF:FF:FF:FF"),
-            "--mqtt_broker", config.get("mqtt_broker", "localhost"),
-            "--mqtt_port", config.get("mqtt_port", 1883),
-            "--mqtt_clientid", config.get("mqtt_clientid", "mqtt2bisecur"),
-            "--mqtt_username", config.get("mqtt_username", "bisecur"),
-            "--mqtt_password", config.get("mqtt_password", "bisecur"),
-            "--mqtt_tls", config.get("mqtt_tls", None),
-            "--mqtt_topic_base", config.get("mqtt_topic_base", "bisecur2mqtt"),
-            "--mqtt_topic_HA_discovery", config.get("mqtt_topic_HA_discovery", "homeassistant"),
-            "--logfile", config.get("logfile", "mqtt2bisecur.log"),
-            "--logs", config.get("logs", False)
+            "--bisecur_user", str(config.get("bisecur_user", "")),
+            "--bisecur_pw", str(config.get("bisecur_pw", "")),
+            "--bisecur_ip", str(config.get("bisecur_ip", "")),
+            "--bisecur_mac", str(config.get("bisecur_mac", "FF:FF:FF:FF:FF:FF")),
+            "--src_mac", str(config.get("src_mac", "FF:FF:FF:FF:FF:FF")),
+            "--mqtt_broker", str(config.get("mqtt_broker", "localhost")),
+            "--mqtt_port", str(config.get("mqtt_port", 1883)),
+            "--mqtt_clientid", str(config.get("mqtt_clientid", "mqtt2bisecur")),
+            "--mqtt_username", str(config.get("mqtt_username", "bisecur")),
+            "--mqtt_password", str(config.get("mqtt_password", "bisecur")),
+            "--mqtt_tls", str(config.get("mqtt_tls", None)),
+            "--mqtt_topic_base", str(config.get("mqtt_topic_base", "bisecur2mqtt")),
+            "--mqtt_topic_HA_discovery", str(config.get("mqtt_topic_HA_discovery", "homeassistant")),
+            "--logfile", str(config.get("logfile", "mqtt2bisecur.log")),
+            "--logs", str(config.get("logs", False))
         ]
 
         process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
